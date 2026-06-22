cec202432e2acd482b594b21e52e9947d0ddee2e
AkkalDhami <dhamiakkal21@gmail.com>
2026-06-22 15:37:43 +0545

    feat(service-section): enhance service offerings with new titles and descriptions feat(sheet): add Sheet component for improved UI dialog management

--- components/service/service-setion.tsx
+++ components/service/service-setion.tsx
@@ -6,6 +6,7 @@ import {
   IconWorld,
   IconTopologyStar3,
   IconComponents,
+  IconSketching,
 } from "@tabler/icons-react"
 import { ServiceCard } from "./service-card"
 import { Section } from "@/components/ui/section"
@@ -19,30 +20,38 @@ export const services = [
     description:
       "Crafting intuitive and visually engaging digital experiences.",
     icon: IconBrush,
-    className: "md:col-span-1 md:row-span-1",
+    className: "md:col-span-1",
   },
   {
-    title: "Mobile Apps",
-    description: "Designing delightful iOS and Android applications.",
+    title: "Mobile App Design",
+    description: "Designing captivating and user-friendly mobile apps.",
     icon: IconDeviceMobile,
     className: "md:col-span-1 md:row-span-1",
   },
   {
     title: "Website Design",
-    description: "Modern responsive websites built with usability in mind.",
+    description:
+      "I craft modern, responsive web designs that tell your brand's story.",
     icon: IconWorld,
-    className: "md:col-span-2 md:row-span-1",
-  },
-  {
-    title: "Prototyping",
-    description: "Interactive prototypes for rapid testing and validation.",
-    icon: IconTopologyStar3,
     className: "md:col-span-1",
   },
   {
-    title: "Design Systems",
-    description: "Scalable component libraries and design consistency.",
+    title: "Logo Design",
+    description:
+      "Crafting distinctive, scalable, and impactful logos that embody your brand's identity and vision.",
+    icon: IconSketching,
+    className: "md:col-span-1",
+  },
+  {
+    title: "Landing Page Design",
+    description: "Designing captivating and user-friendly landing pages.",
     icon: IconComponents,
+    className: "md:col-span-1",
+  },
+  {
+    title: "Wireframing & Prototyping",
+    description: "Test and refine digital products through wireframing and prototyping.",
+    icon: IconTopologyStar3,
     className: "md:col-span-1",
   },
 ]
